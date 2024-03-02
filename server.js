3ecfd84a98d6dc8cc3d5a1a1b98c2d061bbd6f6b
PatrickDaravong <50096469+PatrickDaravong@users.noreply.github.com>
2024-03-02 20:56:41 +1100

    Scan endpoint

--- server.js
+++ server.js
@@ -10,10 +10,36 @@ app.use(bodyParser.json());
 
 // Endpoint for barcode scanning
 app.post('/scan', (req, res) => {
-  const { barcode, name, price } = req.body;
+  const { barcode } = req.body;
 
-  db.get('SELECT * FROM items WHERE barcode = ?', [barcode], (err, row) => {
-    // Rest of your scanning and database update logic
+  db.get('SELECT * FROM items WHERE barcode = ?', [barcode], (err, itemRow) => {
+    if (err){
+        return res.status(500).json({error: "Server Error"});
+    }
+
+    if (!itemRow) {
+      // need to add an add update item row
+        return res.status(404).json({error: 'Item not found'});
+    }
+
+    db.all('SELECT stores.Name as StoreName, prices.Price, prices.Timestamp' +
+    'FROM prices' +
+    'JOIN stores on prices.StoreID = stores.ID'+
+    'WHERE prices.ItemID = ?' +
+    'GROUP BY prices.StoreID', [itemRow.ID],(pricesErr, priceRows) => {
+      if (priceErr) {
+        return res.status(500).json({ error: 'Error querying prices'});
+      }
+      return res.status(200).json({
+        item: {
+          ID: itemRow.ID,
+          Barcode: itemRow.barcode,
+          Name: itemRow.Name,
+        },
+        prices: priceRows,
+      });
+
+    });
   });
 });
 
