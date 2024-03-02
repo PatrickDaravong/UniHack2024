5bf2894dac12a3fd2adc05e78abd58448d0ebb6c
PatrickDaravong <50096469+PatrickDaravong@users.noreply.github.com>
2024-03-02 21:07:43 +1100

    quaggaJS package for barcode scanner

--- db.js
+++ db.js
@@ -9,8 +9,23 @@ db.run(`
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
     Barcode TEXT UNIQUE,
     Name TEXT,
-    Price DECIMAL(10, 2)
   )
+
+  CREATE TABLE IF NOT EXIST store (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT
+    Name TEXT Unique,
+  )
+
+  CREATE TABLE IF NOT EXIST prices (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT
+    ItemID INTEGER,
+    StoreID INTEGER,
+    Price DECIMAL(10,2)
+    Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREGIN KEY (ItemID) References item (ID),
+    FOREGIN KEY (StoreID) References stores (ID)
+  )
+
 `);
 
 module.exports = db;
